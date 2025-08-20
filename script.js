ae98b6b5cc6d4175bfe8fe9b16e51ecf7aefe265
Ruey-Day <rueyday1@gmail.com>
2025-08-20 23:18:29 +0800

    update teaching section later

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const workBtn = document.getElementById('workBtn');
 const eduBtn = document.getElementById('eduBtn');
+const teachBtn = document.getElementById('teachBtn');
 const workContent = document.getElementById('workExperience');
 const eduContent = document.getElementById('eduExperience');
+const teachContent = document.getElementById('teachExperience');
 
 workBtn.addEventListener('click', () => {
   workBtn.classList.add('active');
   eduBtn.classList.remove('active');
+  teachBtn.classList.remove('active');
   workContent.classList.add('show');
   eduContent.classList.remove('show');
+  teachContent.classList.remove('show');
 });
 
 eduBtn.addEventListener('click', () => {
   eduBtn.classList.add('active');
   workBtn.classList.remove('active');
+  teachBtn.classList.remove('active');
   eduContent.classList.add('show');
   workContent.classList.remove('show');
+  teachContent.classList.remove('show');
 });
+
+teachBtn.addEventListener('click', () => {
+  teachBtn.classList.add('active');
+  workBtn.classList.remove('active');
+  eduBtn.classList.remove('active');
+  teachContent.classList.add('show');
+  workContent.classList.remove('show');
+  eduContent.classList.remove('show');
+});
+
 document.querySelectorAll('.nav-secondary a').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
